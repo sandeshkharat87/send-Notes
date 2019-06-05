be94f561ad7529efdba95169916eb60d8cd3659d
sandesh kharat <sandeshkharat040@gmail.com>
2019-06-05 17:37:05 +0530

    firt version of sendNotes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CLI_PC_PB
 msg pc to smartphone
+
+use >
+
+1) python3.6
+2)pushbullet ( pip install pushbullet.py )
---
+++ sendNotes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.6
+from pushbullet import Pushbullet
+import sys
+
+pb = Pushbullet("api_key")
+
+if len(sys.argv) > 1:
+    notes = " ".join(sys.argv[1:])
+    push = pb.push_note(notes, " ")
+else:
+    sys.exit()
